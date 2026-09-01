a1fa9cf3a5f77071f2f6b3638607a48eca90039c
TR-ASHcoder <aymanraf7@gmail.com>
2026-09-01 18:33:41 +1000

    new lavalink lmao

--- main.py
+++ main.py
@@ -28,11 +28,10 @@ STATUSES = [
     "Good Threads before good threads"
 ]
 
-LAVALINK_HOST = "lavalinkv4.serenetia.com"
-LAVALINK_PORT = 80
-LAVALINK_PASSWORD = "https://seretia.link/discord"
+LAVALINK_HOST = "lava2.kasawa.pro"
+LAVALINK_PORT = 2334
+LAVALINK_PASSWORD = "youshallnotpass"
 LAVALINK_SECURE = False
-
 
 async def ch_pr():
     await bot.wait_until_ready()
