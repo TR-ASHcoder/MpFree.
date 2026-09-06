005546f5e6433cd71f5544094847397759c7772a
TR-ASHcoder <aymanraf7@gmail.com>
2026-09-06 16:44:05 +1000

    using backup node until then playlists wont be able to be added to queue :(

--- main.py
+++ main.py
@@ -31,11 +31,15 @@ STATUSES = [
     "Never Gonna Give You Up by Rick Astley",
 ]
 
-LAVALINK_HOST = "lava2.kasawa.pro"
-LAVALINK_PORT = 2334
-LAVALINK_PASSWORD = "youshallnotpass"
-LAVALINK_SECURE = False
+#LAVALINK_HOST = "lava2.kasawa.pro"
+#LAVALINK_PORT = 2334
+#LAVALINK_PASSWORD = "youshallnotpass"
+#LAVALINK_SECURE = False
 
+LAVALINK_HOST = "lavalinkv4.serenetia.com"
+LAVALINK_PORT = 80
+LAVALINK_PASSWORD = "https://seretia.link/discord"
+LAVALINK_SECURE = False
 
 async def ch_pr():
     await bot.wait_until_ready()
